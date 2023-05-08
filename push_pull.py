abe23c556d30a59934bd957a6055820e66dc197a
Brendanlearnings <brendanlearnings@gmail.com>
2023-05-08 11:11:48 +0200

    Keep cursor alive

--- push_pull.py
+++ push_pull.py
@@ -29,10 +29,11 @@ if st.button('Submit'):
         st.write('Your data has been successfully captured!')
 
 
-    data = cur.execute('SELECT COUNT(1) FROM GRAFANA.DEMO.USER_INFO;')
-    counts = data.fetchall()
-
-    st.write(f'There has been {counts} submissions')
+        data = cur.execute('SELECT COUNT(1) FROM GRAFANA.DEMO.USER_INFO;')
+        counts = cur.fetchall()
+    
+        st.write(f'There has been {counts} submissions')
+        cur.close()
 
     
 
